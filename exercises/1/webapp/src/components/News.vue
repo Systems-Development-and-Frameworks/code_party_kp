de7d3f7102eaa11f18f28e20865299f9d8ddc722
Anton Karakochev <anton.karakochev@moia.io>
2020-10-21 18:58:15 +0200

    finish exercise 1 - create app/components - implement updates via events - implement creation - automatically sort via computed property - make news a data property in NewsList instead of App - use form instead of input for entry creation

---
+++ exercises/1/webapp/src/components/News.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{ news.title }}({{ news.votes }})</h2>
+    <button @click="upvote">Upvote</button>
+    <button @click="downvote">Downvote</button>
+    <button @click="remove">Remove</button>
+  </div>
+</template>
+<script>
+export default {
+  props: { news: Object },
+  methods: {
+    upvote() {
+      this.news.votes += 1;
+      this.$emit("update", this.news);
+    },
+    downvote() {
+      this.news.votes -= 1;
+      this.$emit("update", this.news);
+    },
+    remove() {
+      this.$emit("remove", this.news);
+    },
+  },
+};
+</script>
