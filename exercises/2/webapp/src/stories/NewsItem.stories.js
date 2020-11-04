0b68b4443650ab506d1282dcfb5280444e5f1e14
jingyophuong <s0547007@htw-berlin.de>
2020-11-04 19:43:56 +0100

    fix lint error

--- exercises/2/webapp/src/stories/NewsItem.stories.js
+++ exercises/2/webapp/src/stories/NewsItem.stories.js
@@ -1,23 +1,18 @@
 import News from "../components/News.vue";
-import { linkTo } from '@storybook/addon-links'
-import { action, decorate } from '@storybook/addon-actions'
+//import { action, decorate } from '@storybook/addon-actions'
 
 export default {
     title: 'NewsItem',
     component: News,
     argTypes: { onClick: { action: 'update' } },
   };
-  const news = {
-      id: 1,
-      title: 'This control updates the component in real-time',
-      votes: 42
-  }
-  export const Default = (args, {argTypes}) => ({
+  
+  export const Default = (args, /*{argTypes}*/) => ({
       components: {News}, 
       template: ' <News :news="news" @update="update" @remove="remove" />',
       props: {
         news: {
-            default: ()=> news,
+            default: ()=> args,
         }
       },
   }); 
