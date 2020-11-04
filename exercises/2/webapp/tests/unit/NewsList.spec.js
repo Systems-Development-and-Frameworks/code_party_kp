d73f5c49b612af0d8b87b5a10b28bf83eb32cb79
jingyophuong <s0547007@htw-berlin.de>
2020-11-04 15:08:57 +0100

    implement some test cases

--- exercises/2/webapp/tests/unit/NewsList.spec.js
+++ exercises/2/webapp/tests/unit/NewsList.spec.js
@@ -1,9 +1,8 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import NewsList from '@/components/NewsList.vue'
 
 describe('empty', () => {
     it('render empty state', () => {
-        let slug
         const wrapper = shallowMount(NewsList, {
             data() {
                 return { news: [] }
@@ -22,7 +21,7 @@ describe('not empty', () => {
         })
         expect(wrapper.find('.emptyList').exists()).toBeFalsy();
     })
-    
+
     describe('click "Reserve order"', () => {
         it('toggles between ascending and descending order', () => {
     
