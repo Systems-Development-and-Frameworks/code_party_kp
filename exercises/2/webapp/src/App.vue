4898cab02addfc442a624dfadecfd8ca81f97ad8
kipmann <kirill_prahov@mail.ru>
2020-11-18 16:10:15 +0100

    Revert "copy from the branch Exercise2"
    This reverts commit 55a075cb859ae168914d57073c7b26e3de2612ce.

---
+++ exercises/2/webapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>News List</h1>
+    <NewsList />
+  </div>
+</template>
+
+<script>
+import NewsList from "./components/NewsList";
+
+export default {
+  name: "App",
+  components: {
+    NewsList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
