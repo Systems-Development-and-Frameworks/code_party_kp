a8ba5d0d649f6f9a733f1539c743a04768361dc4
jingyophuong <s0547007@htw-berlin.de>
2020-11-18 17:37:48 +0100

    Change the constructor of the component. - create db.js for the datasource - create server.js for the resolve command and server class - create typeDefs.js for the schema - implement class contructors for Post and User

---
+++ backend/db.js
@@ -0,0 +1,65 @@
+import { DataSource } from 'apollo-datasource'
+import crypto from 'crypto'
+
+export class User{
+    constructor (data){
+       Object.assign(this,data)
+    }
+}
+export class Post {
+  constructor (data) {
+    this.id = crypto.randomBytes(16).toString('hex')
+    this.votes = 0
+    this.author = new User(data.author)
+    this.voters = new Set()
+    Object.assign(this, data)
+  }
+}
+
+export class MemoryDataSource extends DataSource {
+    //TODO  constructor injection
+  
+    constructor() {
+      super();
+  
+      this.usersData = new Set();
+      this.postsData = [];
+    }
+  
+    initialize() {}
+  
+    addnewPost(postInput) {
+      let post = new Post(postInput);
+      
+      //it's a set, doesn't matter if exists or not
+      this.usersData.add(postInput.author);
+  
+      //posts can have duplicate content, as long as id's are different, so no checks needed
+      this.postsData.push(post);
+      return post;
+    }
+  
+    async posts() {
+      return this.postsData;
+    }
+    async getPosts(id) {
+      return this.postsData.filter((x) => x.author.name == id);
+    }
+  
+    async users() {
+      return this.usersData;
+    }
+    async upvote(id, voter) {
+      let postIndex = this.postsData.findIndex((x) => x.id == id);
+      if(postIndex != -1)
+      {
+        const currentPost =  this.postsData[postIndex];
+        currentPost.voters.add(voter);
+        currentPost.votes = currentPost.voters.size;
+        return currentPost;
+      }
+      return undefined;
+    }
+   
+  }
+  
