72dccc51d40a99a36a0b6fc646d45b5f66e083e1
jingyophuong <s0547007@htw-berlin.de>
2020-11-25 17:02:14 +0100

    try to configure classes but still have a problem with user class

--- backend/typeDefs.js
+++ backend/typeDefs.js
@@ -1,48 +1,48 @@
 import { gql } from "apollo-server";
 
 const typeDefs = gql`
-type Post {
-  id: ID!
-  title: String!
-  votes: Int!
-  author: User!
-}
+  type Post {
+    id: ID!
+    title: String!
+    votes: Int!
+    author: User!
+  }
 
-type User {
-  # ⚠️ attributes 'id' and 'name' have changed!
-  # 'id' now represents a randomly generated string, similar to 'Post.id'
-  id: ID!
-  name: String!
-  email: String!
-  posts: [Post]
-}
+  type User {
+    # ⚠️ attributes 'id' and 'name' have changed!
+    # 'id' now represents a randomly generated string, similar to 'Post.id'
+    id: ID!
+    name: String!
+    email: String!
+    posts: [Post]
+  }
 
-type Query {
-  posts: [Post]
-  users: [User]
-}
+  type Query {
+    posts: [Post]
+    users: [User]
+  }
 
-type Mutation {
-  write(post: PostInput!): Post
-  upvote(id: ID!): Post
-  # 🚀 OPTIONAL
-  # downvote(id: ID!): Post
-  # 🚀 OPTIONAL
-  # delete(id: ID!): Post
+  type Mutation {
+    write(post: PostInput!): Post
+    upvote(id: ID!): Post
+    # 🚀 OPTIONAL
+    # downvote(id: ID!): Post
+    # 🚀 OPTIONAL
+    # delete(id: ID!): Post
 
-  """
-  returns a signed JWT or null
-  """
-  login(email: String!, password: String!): String
+    """
+    returns a signed JWT or null
+    """
+    login(email: String!, password: String!): String
 
-  """
-  returns a signed JWT or null
-  """
-  signup(name: String!, email: String!, password: String!): String
-}
+    """
+    returns a signed JWT or null
+    """
+    signup(name: String!, email: String!, password: String!): String
+  }
 
-input PostInput {
-  title: String!
-}
+  input PostInput {
+    title: String!
+  }
 `;
 export default typeDefs;
