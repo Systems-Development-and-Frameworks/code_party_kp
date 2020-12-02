c17ff08a83f257fd77322e4934547f54e7fc24cd
Anton Karakochev <anton.karakochev@moia.io>
2020-12-02 15:01:20 +0200

    Add graphql-middleware - glue together middleware and shield - add to context

---
+++ backend/permissions.js
@@ -0,0 +1,16 @@
+// Rules
+import { rule, shield, and, or, not } from "graphql-shield";
+
+const isAuthenticated = rule({ cache: "contextual" })(
+  async (parent, args, { dataSources }, info) => {
+    console.log("ASDFG");
+    return dataSources.db.user !== null;
+  }
+);
+
+export const permissions = shield({
+  Mutation: {
+    upvote: isAuthenticated,
+    write: isAuthenticated,
+  },
+});
