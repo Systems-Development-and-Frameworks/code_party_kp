af16fffff5ac6330891dd98cef2b0f90ba97b3e6
jingyophuong <s0547007@htw-berlin.de>
2020-12-28 17:29:56 +0100

    correcting query of posts

--- backend/src/resolvers/query_all.js
+++ backend/src/resolvers/query_all.js
@@ -1,22 +1,17 @@
 import { neo4jgraphql } from "neo4j-graphql-js";
 
-export default ({ subschema }) => ({
+export default () => ({
   Query: {
     users: async (_parent, _args, context, resolveInfo) => {
-      //return context.dataSources.db.users();
+    
       return neo4jgraphql(_parent, _args, context, resolveInfo);
     },
-    posts: async (_parent, _args, context) => {
-      //TODO make it in neo4jgraphql
-      return context.dataSources.db.posts();
+    posts: async (_parent, _args, context,resolveInfo) => {
+    
+      return neo4jgraphql(_parent, _args, context, resolveInfo);
     },
   },
-  User: {
-    posts: async (parent, _args, context) => {
-      //TODO make it in neo4jgraphql
-      return context.dataSources.db.getPosts(parent.id);
-    },
-  },
+  
   //TODO not tested yet
   Post: {
     votes: {
