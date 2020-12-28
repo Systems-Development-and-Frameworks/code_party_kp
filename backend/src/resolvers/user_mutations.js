0c7b6600a4b5567df0e5e1df176fc4e26b3595d4
jingyophuong <s0547007@htw-berlin.de>
2020-12-28 22:25:20 +0100

    implementing user and post mutation

--- backend/src/resolvers/user_mutations.js
+++ backend/src/resolvers/user_mutations.js
@@ -1,6 +1,8 @@
 const hashing = require("../services/hashing.js");
 const jwt = require("../services/jwt.js");
 import { AuthenticationError, UserInputError } from "apollo-server";
+import User from '../db/entities/User';
+
 
 function isPasswordStrong(password) {
   return password.length >= 8;
@@ -9,31 +11,32 @@ function isPasswordStrong(password) {
 //TODO make it in neo4jgraphql
 export default ({ subschema }) => ({
   Mutation: {
-    login: async (parent, args, context) => {
-      let user = await context.dataSources.db.getUserByEmail(args.email);
-      if (!user)
+    login: async (parent, {email, password}, context) => {
+      const user = await User.first({email});
+      if (!user) {
         throw new UserInputError(
           "There is no user registered with this Email!"
         );
-      const isPasswordCorrect = await user.checkPassword(args.password);
+      }
+      const isPasswordCorrect = await user.checkPassword(password);
       if (!isPasswordCorrect)
         throw new AuthenticationError("Password did not match!");
       return jwt.issueToken(user.id);
     },
 
-    signup: async (parent, args, context) => {
-      let emailExists = await context.dataSources.db.emailExists(args.email);
+
+    signup: async (parent, {name, email, password}, context) => {
+      let emailExists = await User.exists({email});
       if (emailExists) throw new UserInputError("Email already exists!");
-      if (!isPasswordStrong(args.password))
+      if (!isPasswordStrong(password))
         throw new UserInputError(
           "Password must be at least 8 characters long!"
         );
-      let encryptedPassword = await hashing.hash(args.password);
-      let user = await context.dataSources.db.addNewUser({
-        name: args.name,
-        email: args.email,
-        password: encryptedPassword,
+      const user = new User({
+        name: name,
+        email: email,
       });
+      await user.save(password);
       return jwt.issueToken(user.id);
     },
   },
