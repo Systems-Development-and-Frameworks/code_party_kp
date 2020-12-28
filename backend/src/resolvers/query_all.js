757b80039ffc30d63a22d0b484b070846a25f486
Anton Karakochev <anton.karakochev@moia.io>
2020-12-28 18:31:58 +0200

    fix votes undefined

--- backend/src/resolvers/query_all.js
+++ backend/src/resolvers/query_all.js
@@ -2,21 +2,19 @@ import { neo4jgraphql } from "neo4j-graphql-js";
 
 export default () => ({
   Query: {
-    users: async (_parent, _args, context, resolveInfo) => {
-    
-      return neo4jgraphql(_parent, _args, context, resolveInfo);
+    users: async (parent, args, context, resolveInfo) => {
+      return neo4jgraphql(parent, args, context, resolveInfo);
     },
-    posts: async (_parent, _args, context,resolveInfo) => {
-    
-      return neo4jgraphql(_parent, _args, context, resolveInfo);
+    posts: async (parent, args, context, resolveInfo) => {
+      return neo4jgraphql(parent, args, context, resolveInfo);
     },
   },
-  
+
   //TODO not tested yet
   Post: {
     votes: {
       selectionSet: "{ voters { id } }",
-      resolve: (post) => post.voters.length,
+      resolve: (post) => post.voters?.length ?? 0,
     },
   },
 });
