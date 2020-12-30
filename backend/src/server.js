fe8fa53c36e349ff2c3543ee1fff33f55d73916a
Anton Karakochev <anton.karakochev@moia.io>
2020-12-30 17:05:27 +0200

    reapply middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,6 @@ import typeDefs from "./typeDefs.js";
 import { permissions } from "./permissions.js";
 import { context } from "./context.js";
 import { applyMiddleware } from "graphql-middleware";
-import { makeExecutableSchema } from "graphql-tools";
 import { mergeResolvers } from "@graphql-tools/merge";
 import postsMutationResolver from "./resolvers/mutations_post";
 import usersMutationResolver from "./resolvers/user_mutations";
@@ -13,18 +12,12 @@ import { stitchSchemas } from "@graphql-tools/stitch";
 
 import neo4jSchema from "./neo4j-graphql-js/schema";
 
-//const db = new MemoryDataSource();
-//const dataSources = () => ({ db });
-
 //load all resolvers and combine them
 const resolvers = mergeResolvers([
   postsMutationResolver({ subschema: neo4jSchema }),
   usersMutationResolver({ subschema: neo4jSchema }),
   queryResolver({ subschema: neo4jSchema }),
 ]);
-
-//combine into executable schema
-//const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const schema = stitchSchemas({
   subschemas: [neo4jSchema],
@@ -33,13 +26,13 @@ const schema = stitchSchemas({
 });
 
 //enrichen with middleware
-//const schemaWithMiddleWare = applyMiddleware(schema, permissions);
+const schemaWithMiddleWare = applyMiddleware(schema, permissions);
 
 //create an instance of Apollo Server
 export default class Server {
   constructor(opts) {
     const defaults = {
-      schema: schema,
+      schema: schemaWithMiddleWare,
       context,
     };
     return new ApolloServer({ ...defaults, ...opts });
