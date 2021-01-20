df059e498e89eac8995d1955c1889be4f0d45cad
kipmann <s0554624@htw-berlin.de>
2021-01-20 18:11:01 +0100

    Exercise7 add login routing (#22)
    * add login routing, view news only for logged users
    
    * update navbar
    
    * Update LoginForm.vue
    
    * Update auth.js

--- webapp/components/NewsForm.vue
+++ webapp/components/NewsForm.vue
@@ -1,11 +1,12 @@
 <template>
   <form @submit.prevent>
     <input v-model="newTitle" aria-label="title" />
-    <button @click="click" :disabled="!newTitle">Create</button>
+    <button @click="click" :disabled="!newTitle" v-if="isAuthenticated">Create</button>
   </form>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
 export default {
   data() {
     return {
@@ -19,5 +20,8 @@ export default {
       this.$emit("create", item);
     },
   },
+  computed: {
+     ...mapGetters('auth', ['isAuthenticated'])
+  }
 };
 </script>
