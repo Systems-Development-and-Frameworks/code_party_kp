28b69b26775b7b42a3b87a7d8acb49a8b0e69f51
jingyophuong <s0547007@htw-berlin.de>
2021-01-28 00:24:41 +0100

    small change on style of footer

--- webapp/components/Footer.vue
+++ webapp/components/Footer.vue
@@ -1,8 +1,8 @@
 <template >
-  <footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
+  <footer class="footer relativ bg-gray-800 pt-10 sm:mt-10 pt-10" >
     <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
       <!-- Col-1 -->
-      <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+      <div class="px-5 w-1/2 sm:w-4/12 md:w-3/12">
         <!-- Col Title -->
         <div class="text-xs uppercase text-gray-400 font-medium mb-6">
           Getting Started
@@ -17,7 +17,7 @@
         </a>
       </div>
       <!-- Col-2 -->
-      <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+      <div class="px-5 w-1/2 sm:w-4/12 md:w-3/12">
         <!-- Col Title -->
         <div class="text-xs uppercase text-gray-400 font-medium mb-6">
           News & Info
@@ -32,7 +32,7 @@
         </a>
       </div>
       <!-- Col-3 -->
-      <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+      <div class="px-5 w-1/2 sm:w-4/12 md:w-3/12">
         <!-- Col Title -->
         <div class="text-xs uppercase text-gray-400 font-medium mb-6">
           Community
@@ -47,7 +47,7 @@
         </a>
       </div>
        <!-- Col-4 -->
-      <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+      <div class="px-5 w-1/2 sm:w-4/12 md:w-3/12">
         <!-- Col Title -->
         <div class="text-xs uppercase text-gray-400 font-medium mb-6">
           Help & FAQs
@@ -75,12 +75,4 @@
     </div>
   </footer>
 </template> 
-
-<style>
-footer {
-  bottom: 0;
-  position: fixed;
-  width: 100%;
-}
-</style>
 
