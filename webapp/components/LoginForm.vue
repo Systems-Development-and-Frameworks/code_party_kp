1a7ca3d359d16407709ca953431017e33122f301
jingyophuong <s0547007@htw-berlin.de>
2021-01-28 00:26:44 +0100

    implement fontend for login form

--- webapp/components/LoginForm.vue
+++ webapp/components/LoginForm.vue
@@ -1,21 +1,65 @@
+
 <template>
-  <h1 class="info" v-if="isAuthenticated">Successfully logged in!</h1>
-  <form v-else @submit.prevent="submit">
-    <label>Email</label>
-    <input id="email" v-model="email" aria-label="email" />
-    <label>Password</label>
-    <input
-      id="password"
-      v-model="password"
-      aria-label="password"
-      type="password"
-    />
-    <button :disabled="!(email && password)" type="submit">
-      Login
-    </button>
+  <div>
+    <h1 class="p-10 text-xl mx-auto md:w-full md:max-w-md info" v-if="isAuthenticated">Successfully logged in!</h1>
+    <form
+      class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12"
+      v-else
+      @submit.prevent="submit"
+    >
+      <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div class="px-5 py-7">
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >E-mail</label
+            >
+            <input
+              type="text"
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              id="email"
+              v-model="email"
+              aria-label="email"
+            />
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >Password</label
+            >
+            <input
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              id="password"
+              v-model="password"
+              aria-label="password"
+              type="password"
+            />
+            <button
+              class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+              :disabled="!(email && password)"
+              type="submit"
+            >
+              <span class="inline-block mr-2">Login</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="w-4 h-4 inline-block"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
     <div v-if="error" class="error">{{ error }}</div>
-  </form>
+  </div>
 </template>
+
+
 
 <script>
 import { mapActions, mapGetters } from "vuex";
@@ -25,7 +69,7 @@ export default {
       //TODO remove later
       email: "peter@widerstand-der-pinguine.ev",
       password: "peter1",
-      error: ""
+      error: "",
     };
   },
   computed: mapGetters("auth", ["isAuthenticated"]),
@@ -35,15 +79,15 @@ export default {
       try {
         const success = await this.login({
           email: this.email,
-          password: this.password
+          password: this.password,
         });
         if (success) this.error = "";
         else this.error = "Incorrect email/password!";
       } catch (err) {
         this.error = "Internal ERROR: Something went wrong!";
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
