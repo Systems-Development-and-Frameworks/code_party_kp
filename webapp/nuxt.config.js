8635347618eec9af728ccacce81166509a9d1d28
jingyophuong <s0547007@htw-berlin.de>
2021-01-28 00:25:27 +0100

    add css file

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -50,7 +50,7 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    
+    '@/assets/tailwind.css',
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
