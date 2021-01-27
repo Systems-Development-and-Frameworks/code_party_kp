90f62965f3c334141bf18d2b79a958c91ea921e2
jingyophuong <s0547007@htw-berlin.de>
2021-01-28 00:29:12 +0100

    styling der items and responsive content section

--- webapp/components/NewsItem.vue
+++ webapp/components/NewsItem.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div>
     <h2>{{ news.title }}({{ news.votes }})</h2>
     <button @click="upvote" v-if="isAuthenticated">Upvote</button>
@@ -6,6 +6,63 @@
     <button v-if="isAuthor">Delete</button>
     <button v-if="isAuthor">Edit</button>
   </div>
+</template>-->
+<template>
+    <div
+      class="flex flex-col items-stretch min-h-full pb-4 mb-6 transition-all duration-150 bg-white rounded-lg shadow-lg hover:shadow-2xl"
+    >
+      
+      <hr class="border-gray-300" />
+      <div class="flex flex-wrap items-center flex-1 px-4 py-1 text-center mx-auto">
+        <a href="#" class="hover:underline">
+          <h2 class="text-2xl font-bold tracking-normal text-gray-800">
+           {{ news.title }}({{ news.votes }})
+          </h2>
+        </a>
+      </div>
+      <hr class="border-gray-300" />
+      <p
+        class="flex flex-row flex-wrap w-full px-4 py-2 overflow-hidden text-sm text-justify text-gray-700"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, magni
+        fugiat, odit incidunt necessitatibus aut nesciunt exercitationem aliquam
+        id voluptatibus quisquam maiores officia sit amet accusantium aliquid
+        quo obcaecati quasi.
+      </p>
+      <hr class="border-gray-300" />
+       <section class="px-4 py-2 mt-2">
+        <div class="flex items-center justify-between">
+          <div class="flex items-center flex-1">
+            <button
+            class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey"
+            @click="upvote"
+            v-if="isAuthenticated"
+          >
+            Upvote
+          </button>
+          <button
+            class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey"
+            v-if="isAuthenticated"
+          >
+            Downvote
+          </button>
+          <button
+            class="flex-no-shrink p-2 ml-4 mr-2 border-2 border-red-400 rounded hover:text-white text-grey border-grey hover:bg-grey"
+            v-if="isAuthor"
+          >
+            Delete
+          </button>
+          <button
+            class="flex-no-shrink p-2 ml-4 mr-2 border-2 border-green-400 rounded hover:text-white text-grey border-grey hover:bg-grey"
+            v-if="isAuthor"
+          >
+            Edit
+          </button>
+        </div>
+        </div>
+      </section>
+    </div>
+
 </template>
 <script>
 import { mapGetters } from "vuex";
@@ -15,13 +72,13 @@ export default {
   methods: {
     async upvote() {
       this.$emit("upvote", this.news);
-    }
+    },
   },
   computed: {
     ...mapGetters("auth", ["isAuthenticated", "currentUser"]),
     isAuthor() {
       return this.news.author.id === this.currentUser;
-    }
-  }
+    },
+  },
 };
 </script>
